6930b20544d121f278df65e8c7aa53290cdc7695
JJyun-ustc <3382655719@qq.com>
2026-04-18 22:06:24 +0800

    ui_pro

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,3 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
 export const SITE_TITLE = 'YUNN-BLOG';
-export const SITE_DESCRIPTION = '芠芸的发癫网页，记录点滴，分享随笔。';
-export const SITE_AUTHOR = '芠芸';
+export const SITE_DESCRIPTION = 'Yunn 的个人博客，记录点滴，分享随笔。';
+export const SITE_AUTHOR = 'Yunn';
